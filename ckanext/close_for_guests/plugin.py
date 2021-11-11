d9ace3a55ec809c0b73a6c896134196059ccaff4
pooya.oa <pooya.oladazimi@tib.eu>
2021-11-11 16:29:02 +0100

    added webassets and the custom login page for guests

--- ckanext/close_for_guests/plugin.py
+++ ckanext/close_for_guests/plugin.py
@@ -20,6 +20,7 @@ class CloseForGuestsPlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('public/statics', 'ckanext-close-for-guests')
         
 
     #ITemplateHelpers
