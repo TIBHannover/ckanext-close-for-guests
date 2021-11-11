b69c02fa4d1d16ee29232dd353133d75f3899461
pooya.oa <pooya.oladazimi@tib.eu>
2021-11-11 15:22:43 +0100

    added the ckan login function form action

--- ckanext/close_for_guests/plugin.py
+++ ckanext/close_for_guests/plugin.py
@@ -1,9 +1,19 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from flask import redirect
+import ckan.lib.helpers as h
+
+
+def is_user_login():
+    if toolkit.g.user:
+        return True
+    return False
+
 
 
 class CloseForGuestsPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     # IConfigurer
 
@@ -11,3 +21,9 @@ class CloseForGuestsPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         
+
+    #ITemplateHelpers
+
+    def get_helpers(self):
+        return {'is_user_login': is_user_login
+        }
